7b98099409b56a5d4e5dc2de7bddfb41dd09e7d1
NSStudent <omarmn83@gmail.com>
2024-08-24 00:21:18 +0200

    feat: rename some file and folders

--- Package.swift
+++ Package.swift
@@ -1,23 +1,19 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "fischer-core",
+    name: "FischerCore",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "fischer-core",
-            targets: ["fischer-core"]),
+            name: "FischerCore",
+            targets: ["FischerCore"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "fischer-core"),
+            name: "FischerCore"),
         .testTarget(
-            name: "fischer-coreTests",
-            dependencies: ["fischer-core"]),
+            name: "FischerCoreTests",
+            dependencies: ["FischerCore"]),
     ]
 )
