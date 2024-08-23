f8f21afdd1e28f0620587114339702df23ef3418
NSStudent <omarmn83@gmail.com>
2024-08-24 00:28:25 +0200

    fix: clean commented code

--- Tests/fischer-coreTests/fischer_coreTests.swift
+++ Tests/fischer-coreTests/fischer_coreTests.swift
@@ -1,12 +1,7 @@
 import XCTest
-@testable import fischer_core
+@testable import FischerCore
 
 final class fischer_coreTests: XCTestCase {
     func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
 }
