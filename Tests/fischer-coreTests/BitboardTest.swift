21850b18c3b4dff193435c7265c590ea8a77a5fd
NSStudent <omarmn83@gmail.com>
2024-08-24 17:45:19 +0200

    add more test

--- Tests/fischer-coreTests/BitboardTest.swift
+++ Tests/fischer-coreTests/BitboardTest.swift
@@ -2,16 +2,9 @@ import XCTest
 @testable import FischerCore
 
 final class BitboardTests: XCTestCase {
-//    func testExample() throws {
-//        let bitboard = Bitboard(square: .b1) | (Bitboard(square: .b4)  | Bitboard(square: .h1))
-//        print(bitboard.ascii)
-//        let noFileA: Bitboard = 0xfefefefefefefefe
-//        print(noFileA.ascii)
-//        let nBitboard = bitboard << 9 & noFileA
-//        print(nBitboard.ascii)
-//        print(bitboard.lsbIndex)
-//        print(bitboard.msbIndex)
-//    }
+    
+    let multipleSquareBitboard = Bitboard(squares: [.b1, .h1])
+    let e4Bitboard = Bitboard(square: .e4)
     
     func testBasicInit() {
         XCTAssertEqual(Bitboard(), Bitboard(rawValue: 0))
@@ -41,20 +34,145 @@ final class BitboardTests: XCTestCase {
     }
     
     func testLSBIndex() throws {
-        let multipleSquareBitboard = Bitboard(squares: [.b1, .h1])
         XCTAssertEqual(multipleSquareBitboard.lsbIndex, 1)
         XCTAssertEqual(multipleSquareBitboard.lsbSquare, .b1)
+    }
+    
+    func testMSBIndex() throws {
         XCTAssertEqual(multipleSquareBitboard.msbIndex, 7)
         XCTAssertEqual(multipleSquareBitboard.msbSquare, .h1)
+    }
+    
+    func testPopLSBBitboard() throws {
         var popLSBBitboard = multipleSquareBitboard
         let popedLSB = popLSBBitboard.popLSB()
         XCTAssertEqual(popLSBBitboard, Bitboard(square: .h1))
         XCTAssertEqual(popedLSB, Bitboard(square: .b1))
-        
+    }
+    
+    func testPopMSBBitboard() throws {
         var popMSBBitboard = multipleSquareBitboard
         let popedMSB = popMSBBitboard.popMSB()
         XCTAssertEqual(popMSBBitboard, Bitboard(square: .b1))
         XCTAssertEqual(popedMSB, Bitboard(square: .h1))
     }
     
+    func testPopLSBIndex() throws {
+        var popLSBBitboard = multipleSquareBitboard
+        let popedLSBIndex = popLSBBitboard.popLSBIndex()
+        XCTAssertEqual(popLSBBitboard, Bitboard(square: .h1))
+        XCTAssertEqual(popedLSBIndex, 1)
+    }
+    
+    func testPopMSBIndex() throws {
+        var popMSBBitboard = multipleSquareBitboard
+        let popedMSBIndex = popMSBBitboard.popMSBIndex()
+        XCTAssertEqual(popMSBBitboard, Bitboard(square: .b1))
+        XCTAssertEqual(popedMSBIndex, 7)
+    }
+    
+    func testPopLSBSquare() throws {
+        var popLSBBitboard = multipleSquareBitboard
+        let popedLSBSquare = popLSBBitboard.popLSBSquare()
+        XCTAssertEqual(popLSBBitboard, Bitboard(square: .h1))
+        XCTAssertEqual(popedLSBSquare, .b1)
+    }
+    
+    func testPopMSBSquare() throws {
+        var popMSBBitboard = multipleSquareBitboard
+        let popedMSBSquare = popMSBBitboard.popMSBSquare()
+        XCTAssertEqual(popMSBBitboard, Bitboard(square: .b1))
+        XCTAssertEqual(popedMSBSquare, .h1)
+    }
+    
+    func testIsEmpty() throws {
+        XCTAssertTrue(Bitboard().isEmpty)
+        XCTAssertFalse(multipleSquareBitboard.isEmpty)
+    }
+    
+    func testHasMoreThanOne() throws {
+        XCTAssertFalse(Bitboard().hasMoreThanOne)
+        XCTAssertTrue(multipleSquareBitboard.hasMoreThanOne)
+    }
+    
+    func testCount() throws {
+        XCTAssertEqual(multipleSquareBitboard.count, 2)
+        XCTAssertEqual(Bitboard(file: .a).count, 8)
+    }
+    
+    func testShift() throws {
+        XCTAssertEqual(e4Bitboard.shifted(toward: .north), Bitboard(square: .e5))
+        XCTAssertEqual(e4Bitboard.shifted(toward: .south), Bitboard(square: .e3))
+        XCTAssertEqual(e4Bitboard.shifted(toward: .east), Bitboard(square: .f4))
+        XCTAssertEqual(e4Bitboard.shifted(toward: .west), Bitboard(square: .d4))
+        XCTAssertEqual(e4Bitboard.shifted(toward: .northeast), Bitboard(square: .f5))
+        XCTAssertEqual(e4Bitboard.shifted(toward: .southeast), Bitboard(square: .f3))
+        XCTAssertEqual(e4Bitboard.shifted(toward: .northwest), Bitboard(square: .d5))
+        XCTAssertEqual(e4Bitboard.shifted(toward: .southwest), Bitboard(square: .d3))
+    }
+    
+    func testflip() throws {
+        XCTAssertEqual(e4Bitboard.flippedVertically(), Bitboard(square: .e5))
+        XCTAssertEqual(e4Bitboard.flippedHorizontally(), Bitboard(square: .d4))
+    }
+    
+    func testMutableFlip() throws {
+        var horizontalFlip = e4Bitboard
+        horizontalFlip.flipHorizontally()
+        XCTAssertEqual(horizontalFlip, Bitboard(square: .d4))
+        
+        var verticalFlip = e4Bitboard
+        verticalFlip.flipVertically()
+        XCTAssertEqual(verticalFlip, Bitboard(square: .e5))
+    }
+
+    func testMutableShift() throws {
+        var shiftBoard = e4Bitboard
+        shiftBoard.shift(toward: .north)
+        XCTAssertEqual(shiftBoard, Bitboard(square: .e5))
+    }
+    
+    func testFilled() throws {
+        XCTAssertEqual(multipleSquareBitboard.filled(toward: .north, stoppers: e4Bitboard), Bitboard(file: .b) | Bitboard(file: .h))
+    }
+    
+    func testFill() throws {
+        var filled = multipleSquareBitboard
+        filled.fill(toward: .north, stoppers: multipleSquareBitboard)
+        XCTAssertEqual(filled, Bitboard(file: .b) | Bitboard(file: .h))
+    }
+    
+    func testSwap() throws {
+        var swapBitboard = e4Bitboard
+        swapBitboard.swap(.e4, .e5)
+        XCTAssertEqual(swapBitboard, Bitboard(square: .e5))
+    }
+    
+    func testPawnAttacks() throws {
+        XCTAssertEqual(e4Bitboard.attacks(for: .init(pawn: .white)), Bitboard(squares: [.d5, .f5]))
+        XCTAssertEqual(e4Bitboard.attacks(for: .init(pawn: .black)), Bitboard(squares: [.d3, .f3]))
+    }
+    
+    func testKnightAttacks() throws {
+        XCTAssertEqual(e4Bitboard.attacks(for: .init(knight: .white)), Bitboard(squares: [.d6, .f6, .g5, .g3, .f2, .d2, .c3, .c5]))
+    }
+    
+    func testBishopAttacks() throws {
+        XCTAssertEqual(e4Bitboard.attacks(for: .init(bishop: .white)), Bitboard(squares: [.b1, .c2, .d3, .f5, .g6, .h7, .a8, .b7, .c6, .d5, .f3, .g2, .h1 ]))
+    }
+    
+    func testRookAttacks() throws {
+        XCTAssertEqual(e4Bitboard.attacks(for: .init(rook: .white)), Bitboard(file: .e) ^ Bitboard(rank: .four))
+    }
+    
+    func testQueenAttacks() throws {
+        XCTAssertEqual(
+            e4Bitboard.attacks(for: .init(queen: .white)),
+            Bitboard(squares: [.b1, .c2, .d3, .f5, .g6, .h7, .a8, .b7, .c6, .d5, .f3, .g2, .h1 ]) | (Bitboard(file: .e) ^ Bitboard(rank: .four))
+            )
+    }
+    
+    func testKingAttacks() throws {
+        XCTAssertEqual(e4Bitboard.attacks(for: .init(king: .white)), Bitboard(squares: [.d5, .d4, .d3, .e5, .e3, .f5, .f4, .f3]))
+    }
 }
