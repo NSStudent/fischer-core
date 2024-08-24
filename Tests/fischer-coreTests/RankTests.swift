0d33045ad1c3ea6a26b06913de59bd54a2d8c33a
NSStudent <omarmn83@gmail.com>
2024-08-24 12:52:46 +0200

    feat: add Rank

---
+++ Tests/fischer-coreTests/RankTests.swift
@@ -0,0 +1,33 @@
+import XCTest
+@testable import FischerCore
+
+final class RankTests: XCTestCase {
+    func testInit() throws {
+        XCTAssertEqual(Rank(index: 0), .one)
+        XCTAssertEqual(Rank(rawValue: 1), .one)
+    }
+    
+    func testRankIndex() throws {
+        XCTAssertEqual(Rank.one.index, 0)
+    }
+    
+    func testOpposite() throws {
+        XCTAssertEqual(Rank.one.opposite(), .eight)
+    }
+    
+    func testDescription() throws {
+        XCTAssertEqual(Rank.one.description, "1")
+    }
+    
+    func testComparable() throws {
+        XCTAssertTrue(Rank.one < .two)
+        XCTAssertFalse(Rank.one > .two)
+        XCTAssertFalse(Rank.one == .two)
+        XCTAssertTrue(Rank.one == .one)
+    }
+    
+    func testInteger() throws {
+        let rank: Rank = 1
+        XCTAssertEqual(rank, .one)
+    }
+}
