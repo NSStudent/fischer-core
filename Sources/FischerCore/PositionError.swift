c8b049f249787a5eba97d2f4761c3f53e8b69e45
NSStudent <omarmn83@gmail.com>
2025-04-12 21:29:19 +0200

    flatten position model

---
+++ Sources/FischerCore/PositionError.swift
@@ -0,0 +1,17 @@
+//
+//  PositionError.swift
+//  FischerCore
+//
+//  Created by Omar Megdadi on 12/4/25.
+//
+
+public enum PositionError: Error {
+    case wrongKingCount(PlayerColor)
+    case missingKing(CastlingRights.Right)
+    case missingRook(CastlingRights.Right)
+    case wrongEnPassantTargetRank(Rank)
+    case nonEmptyEnPassantTarget(Square, Piece)
+    case missingEnPassantPawn(Square)
+    case nonEmptyEnPassantSquare(Square, Piece)
+    case fenMalformed
+}
