f245c6b231825ecc996e193a0888de32aa098365
NSStudent <omarmn83@gmail.com>
2026-05-15 19:05:56 +0200

    Fix mixed PGN comment annotations

--- Sources/FischerCore/PGN/Parsers/CommentParsers/MultipleCommentParser.swift
+++ Sources/FischerCore/PGN/Parsers/CommentParsers/MultipleCommentParser.swift
@@ -7,24 +7,43 @@
 
 import Parsing
 
+private struct SpecialCommentParser: Parser {
+    var body: some Parser<Substring, PGNComment> {
+        OneOf {
+            CALParser()
+            CSLParser()
+            EMTParser()
+            EvalParser()
+            CLKParser()
+        }
+    }
+}
+
 struct MultipleCommentParser: Parser {
     var body: some Parser<Substring, [PGNComment]> {
-        "{"
-        Whitespace()
-        Many {
-            OneOf{
-                CALParser()
-                CSLParser()
-                EMTParser()
-                EvalParser()
-                CLKParser()
-            }
-        } separator: {
-            OneOf {
+        Parse {
+            "{"
+            Whitespace()
+            SpecialCommentParser()
+            Many {
+                SpecialCommentParser()
+            } separator: {
                 Whitespace()
             }
+            Optionally {
+                Whitespace(1...)
+                Prefix { $0 != "}" }
+                    .map(String.init)
+            }
+            Whitespace()
+            "}"
+        }.map { firstComment, remainingComments, text in
+            var comments = [firstComment]
+            comments.append(contentsOf: remainingComments)
+            if let text, !text.isEmpty {
+                comments.append(.text(text))
+            }
+            return comments
         }
-        Whitespace()
-        "}"
     }
 }
